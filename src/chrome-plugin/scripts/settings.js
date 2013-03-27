54186bc3e2fd83215e6edb0f947d0c8a4a574e63
Tony Chi <qiwei219@gmail.com>
2013-03-27 15:07:18 +0800

    修复getServiceUrl方法中的语法错误

--- src/chrome-plugin/scripts/settings.js
+++ src/chrome-plugin/scripts/settings.js
@@ -8,7 +8,7 @@
 
 var settings = {
     init: function(){
-        setServiceUrl('http://localhost:8082/api/save');
+        this.setServiceUrl('http://localhost:8080/api/save');
     },
     getServiceUrl: function(){
         return localStorage['__service_url'];
