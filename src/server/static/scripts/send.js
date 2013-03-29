9042a8b511dc9ecd81cffbd835f58b1aebe24002
Tony Chi <qiwei219@gmail.com>
2013-03-29 23:43:07 +0800

    为view.html, index.html增加action链接（编辑，删除，发送）；

--- src/server/static/scripts/send.js
+++ src/server/static/scripts/send.js
@@ -7,17 +7,27 @@
  *
  **/
 
-function sendIt(pid, cb) {
-	$.ajax({
-		url: '',
-		type: 'POST',
-		data: { id: pid },
-		success: function(d){
-			cb(d);
-			alert('successed!')
-		},
-		error: function(e){
-			alert(e);
-		}
-	});
-}
+$(document).ready(function(){
+
+    function sendIt(pid) {
+        $.ajax({
+            url: '/send/' + pid,
+            type: 'POST',
+            success: function(d){
+                if(r.success)
+                    alert('已经加入发送队列, 请稍后更新自己的设备更新。');
+                else
+                    alert('对不起, 发送失败!');
+            },
+            error: function(e){
+                console.log(e);
+                alert('对不起，服务器发生故障!');
+            }
+        });
+    }
+
+    $('#action_kindle').click(function(){
+        sendToKindle($(this).attr('data-id'));
+    });
+});
+
